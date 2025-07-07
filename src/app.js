ccef8d3c0fb2fefc3b9c457f867fe399c5962801
Dalton Ousley <daltonjousley@gmail.com>
2025-07-07 13:28:56 -0600

    feat: dialog open and close functionality.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-import "./style.css";
+let notes = [];
+
+function openNoteDialog() {
+  const dialog = document.getElementById("note-dialog");
+  const titleInput = document.getElementById("note-title");
+  const contentInput = document.getElementById("note-content");
+
+  dialog.showModal();
+  titleInput.focus();
+}
+
+function closeNoteDialog() {
+  document.getElementById("note-dialog").close();
+}
